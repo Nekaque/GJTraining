6459834a1e2a80c0eb73ea710057c95c2b3f597d
Nekaque <3217cody@gmail.com>
2025-03-09 10:51:24 +0100

    sync

--- scripts/collision_handler.gd
+++ scripts/collision_handler.gd
@@ -2,11 +2,15 @@ extends Node
 
 var collisions = 0
 var tutorial = true
-
+var music
+var sfx
 
 func _on_music_ready() -> void:
 	$Music.volume_db = 0
+	music = $Music
 
 
 func _on_sfx_ready() -> void:
 	$SFX.volume_db = 0
+	sfx = $SFX
+	
